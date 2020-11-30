4ee7a38b92aadfbbe74e38c1fa93c7f86e0a2d61
enriquop <44776832+enriquop@users.noreply.github.com>
2020-11-30 17:33:46 +0100

    Update dns.bat

--- dns.bat
+++ dns.bat
@@ -1,11 +1,37 @@
 @echo off
 REM SET DNS
-REM SOLO USO EN ORDENADORES CON IDIOMA ESPAÑOL!
 netsh interface ip show config
 pause > nul
 set /p Adaptador="Nombre de adapador: "
 set /p DNS="IP de DNS: "
 netsh interface ipv4 add dnsserver "%Adaptador%" address=%DNS% index=1
-echo Listo!
+echo listo!
 pause
+:start
+SET choice=
+SET /p choice=Deseas poner la dns alternativa? [Y/N]: 
+IF NOT '%choice%'=='' SET choice=%choice:~0,1%
+IF '%choice%'=='Y' GOTO yes
+IF '%choice%'=='y' GOTO yes
+IF '%choice%'=='N' GOTO no
+IF '%choice%'=='n' GOTO no
+IF '%choice%'=='' GOTO no
+ECHO "%choice%" is not valid
+ECHO.
+GOTO start
+
+:no
+cls
+ECHO vas a salir del menu
+timeout 2 > nul
+echo saliendo...
+timeout 1 > nul
+EXIT
+
+:yes
+set /p Adaptador1="Nombre de adapador: "
+set /p DNS2="IP de DNS: "
+netsh interface ipv4 add dnsserver "%Adaptador1%" address=%DNS2% index=2
+echo Ip alternativa asignada correctamente
+timeout 2 > nul
 EXIT
