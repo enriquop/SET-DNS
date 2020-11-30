7e796d334220aa619039b6b78b297dd6375e08b3
enriquop <44776832+enriquop@users.noreply.github.com>
2020-11-30 17:28:59 +0100

    Update dnsgit.bat

--- dnsgit.bat
+++ dnsgit.bat
@@ -4,7 +4,6 @@ netsh interface ip show config
 pause > nul
 set /p Adaptador="Nombre de adapador: "
 set /p DNS="IP de DNS: "
-@REM netsh interface ipv4 set dns name=”Adaptador de Ethernet %Adaptador%” static address=10.254.28.16
 netsh interface ipv4 add dnsserver "%Adaptador%" address=%DNS% index=1
 echo Listo!
 pause
